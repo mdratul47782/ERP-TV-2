97fa656e47893ffae4803c8c67ae94d06c0c52b2
mdratul47782 <mdratul47782@gmail.com>
2025-11-11 19:31:03 +0600

    Updated

--- app/layout.js
+++ app/layout.js
@@ -1,36 +1,36 @@
-import { Geist, Geist_Mono } from "next/font/google";
+// app/layout.js
 import "./globals.css";
 import { dbConnect } from "@/services/mongo";
 import AuthProvider from "./providers/AuthProvider";
 import NavBar from "./components/NavBar";
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import Link from "next/link";
 
 export const metadata = {
   title: "Production Info App",
   description: "HKD Outdoor Innovation Ltd. Production Info App",
 };
 
-export default  async function RootLayout({ children }) {
+export default async function RootLayout({ children }) {
   await dbConnect();
+
   return (
     <html lang="en">
-      
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      ><AuthProvider>
-        <NavBar />
-        {children}
+      <head>
+        {/* ✅ Roboto Condensed + Stack Sans Headline */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
+        <Link
+          href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&family=Stack+Sans+Headline:wght@200..700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+
+      <body className="antialiased">
+        <AuthProvider>
+          <NavBar />
+          {children}
         </AuthProvider>
       </body>
-      
     </html>
   );
 }
