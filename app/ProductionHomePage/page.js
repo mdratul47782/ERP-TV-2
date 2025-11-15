38cb629a5caece782f94407397ee78fd0353dc2a
mdratul47782 <mdratul47782@gmail.com>
2025-11-15 13:49:04 +0600

    Updated

--- app/ProductionHomePage/page.js
+++ app/ProductionHomePage/page.js
@@ -3,7 +3,7 @@ import ProductionSignInOut from "../components/auth/ProductionSignInOut";
 export default function ProductionHomePage(){
     return (
         <div>
-            Production Home Page
+            
             <ProductionSignInOut/>
         </div>
     );
